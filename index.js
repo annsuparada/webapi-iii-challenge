a03846bda14cd2481819a86d176d6ec4acf26791
Suparada Saitalae <ann.suparada@gmail.com>
2019-09-04 10:57:03 -0500

    express running

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code away!
+const express = require('express');
+const server = express();
+
+server.use('/', (req, res) => {
+    res.status(200).send('express running')
+})
+
+server.listen(8000, () => {
+    console.log('server listening to 8000')
+})
